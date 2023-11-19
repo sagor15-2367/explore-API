0d910c3f9b744b97ff6ce5a5b295093fe82a0099
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-11-19 20:07:23 +0600

    ok

---
+++ JS/api.js
@@ -0,0 +1,2 @@
+const user={id:1,name:'Sagor Alam',job:'Engineer'};
+console.log(user);
---
+++ JS/json.js
@@ -0,0 +1,28 @@
+const user={id:1,name:'Sagor Alam',job:'Engineer'};
+// javaScript Object Notation(json)
+const stringField=JSON.stringify(user);
+
+// console.log(user);
+// console.log(stringField);
+
+/*
+{ id: 1, name: 'Sagor Alam', job: 'Engineer' }
+{"id":1,"name":"Sagor Alam","job":"Engineer"}
+*/
+
+const shop={
+    owner:'Sagor Alam',
+    address:{
+        street:'kochukhet',
+        city:'Dhaka',
+        country:'Bangladesh'
+    },
+    products:['laptop','mic','monitor','keyboard'],
+    revenue:45000,
+    isOpen:true,
+    isNew:false
+} 
+const shopJson=JSON.stringify(shop);
+
+// console.log(shop);
+console.log(shopJson);
