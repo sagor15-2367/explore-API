89acb241c6fa9b2231926e80d60ebdc35229e1f3
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-11-28 23:35:51 +0600

    ok

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,3 +1,20 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())// .json is  not similar but close to json.parse
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())// .json is  not similar but close to json.parse
+//       .then(json => console.log(json))
+
+// fetch('')
+
+
+
+// fetch(url)
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+function loadData() {
+      const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+      fetch(url)
+            .then(response => response.json())
+            .then(json => console.log(json))
+
+}
