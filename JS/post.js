6a6ecdfdc515a58eadccc608fc30c1045366c785
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-12-05 16:51:27 +0600

    dynamic data load

---
+++ JS/post.js
@@ -0,0 +1,27 @@
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => displayPosts(data))
+}
+
+// 1. get the container element where you want to add the new element 
+// 2.create child element
+// 3. set innerText/ innerHTML
+// 24. appendChild
+
+
+function displayPosts(posts){
+    const postsContainer=document.getElementById('posts-container');
+    for(const post of posts){
+        const postDiv=document.createElement('div');
+        postDiv.classList.add('post')
+        console.log(post);
+        postDiv.innerHTML=`
+        <h4>User- ${post.userId}</h4>
+        <h5>Post: ${post.title} title</h5>
+        <p>Post Description</p>
+        `;
+        postsContainer.appendChild(postDiv);
+    }
+}
+loadPosts();
